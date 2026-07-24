6395bed8fda21a7e78784ebcf1311f5781eb6495
d3enyuy <gillslambiv@gmail.com>
2026-07-24 21:04:49 +0100

    docs: drop empty doc lines

--- crates/bunkerglow/src/consensus/blockstore/slot_block_data.rs
+++ crates/bunkerglow/src/consensus/blockstore/slot_block_data.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Data structure holding shreds, slices and blocks for a specific slot.
-//!
-//!
 
 use std::collections::BTreeMap;
 use std::collections::btree_map::Entry;
