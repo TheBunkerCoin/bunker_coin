d332c718f793cdc8fd0584412c655b32d3c6b5de
d3enyuy <gillslambiv@gmail.com>
2026-07-29 12:08:17 +0100

    fix: run rotation as the log owner

--- scripts/setup_log_rotation.sh
+++ scripts/setup_log_rotation.sh
@@ -11,6 +11,7 @@ STATE=/var/lib/logrotate/bunkercoin.status
 
 cat > "$CONF" << EOF
 ${HOME_DIR}/bc-node0/run.log ${HOME_DIR}/bc-node1/run.log ${HOME_DIR}/stream.log {
+    su  
     size 200M
     rotate 2
     copytruncate
